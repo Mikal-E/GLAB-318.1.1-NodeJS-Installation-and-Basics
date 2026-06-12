b116b10d227ebdf6eee77d48f635eb3724b95c55
Mikal-E <melamin101@gmail.com>
2026-06-12 13:30:30 -0400

    Parts 1 - 3 completed.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 // Part 2: Node Command Line Interface
 
 console.log("Hello World")
+
+// Part 3: Node Package Manager
+
+// Try modifying the contents of your index.js file, and save it. Watch how the command line responds.
+
+console.log("Using nodemon to run your program is simple")
+
+// Part 4: Creating a Server
+
